dc614c67f346bfddc19ad33fb323d6c9c5a5c782
hiek2001 <hiek2001@naver.com>
2024-09-09 16:38:16 +0900

    설탕배달 : DP

--- src/Baekjoon/sliver/bj2839.java
+++ src/Baekjoon/sliver/bj2839.java
@@ -19,10 +19,12 @@ public class bj2839{
         if(N>=5) D[5] = 1;
         
         for(int i=6 ; i<5001 ; i++) {
-            D[i] = Math.min(D[i-3], D[i-5])+1;
+        	if(D[i-3] != 5001 || D[i-5] != 5001) { // 유효한 값인지 확인
+        		D[i] = Math.min(D[i-3], D[i-5])+1;
+        	}
         }
         
-        if(D[N] > 5001) System.out.println(-1); // 계산되지 않은 값에서 계산된 경우 초기값(5001)보다 큼
+        if(D[N] == 5001) System.out.println(-1); // 5001이면 불가능한 경우
         else System.out.print(D[N]);
     }
 }
